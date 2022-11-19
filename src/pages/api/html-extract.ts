602947364f8db4b05db08bb720fdfdae2c17fc30
Franciszek Madej <franek@alergeek.ventures>
2022-11-19 18:07:29 +0100

    add html extract

---
+++ src/pages/api/html-extract.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { JSDOM } from "jsdom";
+
+// przyjmuje tekst do szukania i zwraca linki do zasobów
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<unknown>
+) {
+  const url = (req.query.url ?? "https://alergeek.ventures/o-nas") as string;
+
+  const response = await fetch(url);
+  const body = await response.text();
+
+  const dom = new JSDOM(body);
+
+  const paragaraphNodes = dom.window.document.querySelectorAll("p");
+  const paragraphs = Array.from(paragaraphNodes).map(
+    (result) => result.textContent ?? ""
+  );
+
+  res.send({
+    paragraphs,
+  });
+}
