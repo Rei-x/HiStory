766dbddfbe948518cea1966e0699920853456a4e
Franciszek Madej <franek@alergeek.ventures>
2022-11-19 18:43:39 +0100

    add DB support

---
+++ src/pages/api/topics.ts
@@ -0,0 +1,43 @@
+import { readFileSync, writeFileSync } from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+import { nanoid } from "nanoid";
+
+const path = "/tmp/db.json";
+
+interface Topic {
+  id: string;
+  title: string;
+  historicalPeriod: string;
+}
+
+export const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  let dbContent = "";
+  try {
+    dbContent = readFileSync(path).toString() ?? "[]";
+  } catch (error) {
+    dbContent = "[]";
+  }
+  const db = JSON.parse(dbContent) as Topic[];
+
+  if (req.method === "DELETE") {
+    const id = req.query.id as string;
+    const indexToDelete = db.findIndex((topic) => topic.id === id);
+    db.splice(indexToDelete, 1);
+  } else if (req.method === "POST") {
+    const topicDto = req.body as { title: string; historicalPeriod: string };
+    db.push({ ...topicDto, id: nanoid() });
+  } else if (req.method === "GET") {
+    res.send({ topics: db });
+  } else {
+    res.send(400);
+    throw new Error("Bad request");
+  }
+
+  const newDbContent = JSON.stringify(db);
+
+  writeFileSync(path, newDbContent);
+
+  res.send("success");
+};
+
+export default handler;
