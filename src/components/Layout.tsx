4c4bbfe307ca2b627f4579fcb9d8b02c61bccc79
Mario Wilk <mario@alergeek.ventures>
2022-11-20 01:59:24 +0100

    add logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
-import { Container, Flex, Link, Text } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
 import NextLink from "next/link";
+import { Container, Flex, HStack, Link, Text, Image } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+import { Logo } from "./Logo";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Flex justifyContent="space-between" mx="auto" mt={8} maxW="container.lg">
-        <Text fontSize="36px" color="#b00909">
-          HiStory
-        </Text>
+        <Flex mr="auto" w="150px">
+          <Logo />
+        </Flex>
         <Link as={NextLink} href="/topics">
           Tematy
         </Link>
---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import { Flex, Image, Link } from "@chakra-ui/react";
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <Image src="/logoHS-szare.png" alt="HiStoryLogo" />
+    </Link>
+  );
+};
